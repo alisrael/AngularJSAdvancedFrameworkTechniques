e84bb775df3486499cda4285961c8de044789044
Annie <alisrael@outlook.com>
2017-01-15 14:00:41 -0600

    Implementing A Session Service, To Implement the Service

--- Mod1Lab/app/sessionService.js
+++ Mod1Lab/app/sessionService.js
@@ -1,4 +1,16 @@
 angular.module('app').services('sessionService', ['$window, sessionService']);
 function serviceSession($window){
-    
+    this.save = save;
+    this.get = get;
+    this.clear = clear;
+    function save(key, value){
+        $window.sessionStorage.setItem(key, value);
+    }
+    function get(key, value){
+        return $window.sessionStorage.getItem(key);
+    }
+    function clear(){
+        $window.sessionStorage.clear();
+    }
+
 }
